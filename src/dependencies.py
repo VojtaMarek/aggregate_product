2ec97981a3a9e0688688ab2c0c1d552da8adc5ee
VojtaMarek <vojtama@gmail.com>
2024-11-14 21:38:53 +0100

    New: service.py for getting access token

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,25 +1,56 @@
+import asyncio
+import os
 import uuid
+from logging import getLogger
 
 from fastapi.params import Depends
-from pydantic.v1.schema import schema
 from sqlalchemy import delete, update
-from sqlalchemy.dialects.mssql.information_schema import sequences
-from sqlalchemy.dialects.mysql import insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from uuid import UUID
 
 from typing import Annotated
 
+import service
 from database import init_db
 import models
 
 
+logger = getLogger(__name__)
 SessionLocal, engine = init_db()
 
 
 async def startup_events():
-    pass
+    # run background tasks
+    loop = asyncio.get_event_loop()
+    loop.create_task(run_periodic_tasks())
+
+
+async def run_periodic_tasks():
+    # periodically request for access token
+    base_url = os.getenv('BASE_URL') + '/auth'
+    service_manager = service.ServiceManager(base_url, os.getenv('REFRESH_TOKEN'), 'POST')
+    while True:
+        try:
+            logger.warning('Attempt to get access token..')
+            res = await service_manager.async_request()
+            # res = service_manager.request()
+
+            # inner loop to repeat request when 400 until 201
+            timer = 1
+            while res.status_code != 201 and res.status_code == 400:
+                logger.warning(f'Repeating attempt to get access token after {timer}s')
+                await asyncio.sleep(timer)
+                res = await service_manager.async_request()
+                # res = service_manager.request()
+                timer *= 2
+
+            if access_token := res.json().get('access_token', False):
+                os.environ['ACCESS_TOKEN'] = access_token
+            await asyncio.sleep(5*60)
+        except Exception as e:
+            logger.error(f'Failed with periodic tasks running. Detail: {e}')
+            raise
 
 
 async def get_db() -> AsyncSession:
---
+++ src/service.py
@@ -0,0 +1,28 @@
+import asyncio
+import requests
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class ServiceManager:
+    def __init__(self, base_url: str, token: str, method:str):
+        self.base_url = base_url
+        self.headers = {
+            'accept': 'application/json',
+            'Bearer': token
+        }
+        # dynamically set request method
+        self.request_method = getattr(requests, method.lower())
+
+    async def async_request(self):
+        try:
+            res = await asyncio.to_thread(self.request_method, self.base_url, headers=self.headers)
+            return res
+        except Exception as e:
+            logger.warning('Request not successful.')
+
+    def request(self):
+        res = self.request_method(self.base_url, headers=self.headers)
+        return res
